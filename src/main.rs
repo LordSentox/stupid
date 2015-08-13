cafb9f4fd2e260b90164744fb675648d5f94c052
Arne Dussin <arne.dussin@live.de>
2015-08-14 00:03:53 +0200

    Now drawing a test grid on startup

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,43 @@
+// TODO: This should be removed when the code is not in the first implementation phase any longer,
+// and must be removed before production, to avoid overhead.
+#![allow(dead_code)]
+
+// This is necessary for handy String-conversions, which are currently awaiting RFC revision.
+#![feature(convert)]
+
 extern crate sdl2;
 
 mod static_object;
-use static_object::StaticObject;
+mod texture_manager;
 
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
 use sdl2::keyboard::Keycode;
 
+fn draw_grid(buffer: &mut [u8], pitch: usize, width: u32, height: u32) {
+    for y in (0..height as usize) {
+        for x in (0..width as usize) {
+            let offset = y*pitch + x*3;
+
+            if (y % 64 == 0) | (x % 64 == 0) {
+                buffer[offset + 0] = 0;
+                buffer[offset + 1] = 0;
+                buffer[offset + 2] = 0;
+            }
+            else {
+                buffer[offset + 0] = 240;
+                buffer[offset + 1] = 240;
+                buffer[offset + 2] = 240;
+            }
+        }
+    }
+}
+
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
+    let window = video_subsystem.window("Powerd by Stupid Engine", 832, 640)
         .position_centered()
         .opengl()
         .build()
@@ -19,22 +45,15 @@ pub fn main() {
 
     let mut renderer = window.renderer().build().unwrap();
 
-    let mut texture = renderer.create_texture_streaming(PixelFormatEnum::RGB24, (256, 256)).unwrap();
-    // Create a red-green gradient
+    let mut texture = renderer.create_texture_streaming(PixelFormatEnum::RGB24, (832, 640)).unwrap();
+    // create a grid, with fields the size 64x64
     texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
-        for y in (0..256) {
-            for x in (0..256) {
-                let offset = y*pitch + x*3;
-                buffer[offset + 0] = x as u8;
-                buffer[offset + 1] = y as u8;
-                buffer[offset + 2] = 0;
-            }
-        }
+        let (width, height) = renderer.output_size().unwrap();
+        draw_grid(buffer, pitch, width, height);
     }).unwrap();
 
     renderer.clear();
-    renderer.copy(&texture, None, Some(Rect::new_unwrap(100, 100, 256, 256)));
-    renderer.copy_ex(&texture, None, Some(Rect::new_unwrap(450, 100, 256, 256)), 30.0, None, (false, false));
+    renderer.copy(&texture, None, Some(Rect::new_unwrap(0, 0, 832, 640)));
     renderer.present();
 
     let mut running = true;
@@ -50,9 +69,15 @@ pub fn main() {
                 },
                 Event::Window {win_event_id: event_id, ..} => {
                     if event_id == sdl2::event::WindowEventId::Resized {
+
+                        let (width, height) = renderer.output_size().unwrap();
+                        let mut texture = renderer.create_texture_streaming(PixelFormatEnum::RGB24, (width, height)).unwrap();
+                        texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+                            draw_grid(buffer, pitch, width, height);
+                        }).unwrap();
+
                         renderer.clear();
-                        renderer.copy(&texture, None, Some(Rect::new_unwrap(100, 100, 256, 256)));
-                        renderer.copy_ex(&texture, None, Some(Rect::new_unwrap(450, 100, 256, 256)), 30.0, None, (false, false));
+                        renderer.copy(&texture, None, Some(Rect::new_unwrap(0, 0, width, height)));
                         renderer.present();
                     }
                 },
