08779e24c51ae442c7ca1edcc37b888376412247
Arne Dussin <arne.dussin@live.de>
2015-08-13 21:48:31 +0200

    StaticObject now uses the TextureManager to allocate resources

--- src/static_object.rs
+++ src/static_object.rs
@@ -7,31 +7,48 @@ extern crate sdl2;
 use self::sdl2::render::{Texture, Renderer};
 use self::sdl2::rect::Rect;
 
+use texture_manager::TextureManager;
+
 pub struct StaticObject<'a> {
 	texture: &'a Texture,
 	// The position is defined by the textures bottommost, leftmost point, since that point is
 	// always "on the ground" or at least nearest to the ground.
 	x: i32,
 	y: i32,
-	width: u16,
-	height: u16
+	width: u32,
+	height: u32
 }
 
 impl<'a> StaticObject<'a> {
-	pub fn new(texture: &'a Texture, x: i32, y: i32, width: u16, height: u16) -> StaticObject {
-		StaticObject {
-			texture: texture,
+	pub fn new(texture_manager: &'a mut TextureManager, texture_name: &str, x: i32, y: i32, width: Option<u32>, height: Option<u32>) -> Result<StaticObject<'a>, String> {
+		let texture_entry = match texture_manager.load_texture(texture_name) {
+			Some(texture_entry) => texture_entry,
+			None => return Err(String::from("Could not load texture."))
+		};
+
+		let width = match width {
+			Some(custom_width) => custom_width,
+			None => texture_entry.width()
+		};
+
+		let height = match height {
+			Some(custom_height) => custom_height,
+			None => texture_entry.height()
+		};
+
+		Ok(StaticObject {
+			texture: texture_entry.texture(),
 			x: x,
 			y: y,
 			width: width,
 			height: height
-		}
+		})
 	}
 
 	// TODO: Move this to a seperate trait, like Drawable in SFML.
 	pub fn draw(&self, renderer: &'a mut Renderer) {
 		// TODO: The textures should be saved in some sort of database, since this is way too
 		// error-prone in many different stages.
-		renderer.copy(self.texture, None, Some(Rect::new_unwrap(self.x, self.y, self.width as u32, self.height as u32)));
+		renderer.copy(self.texture, None, Some(Rect::new_unwrap(self.x, self.y, self.width, self.height)));
 	}
 }
